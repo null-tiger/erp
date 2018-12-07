385253953e3db7df0222558ded44145f39f5925e
mldn <mldnqa@163.com>
2018-12-07 23:37:05 +0800

    Commit Work Dir File

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello GIT !!!") ;
+		//System.out.println("Hello GIT !!!") ;
 		System.out.println("认真听课的孩子才是好孩子！") ;
 		System.out.println("不认真听课的孩子去后面站着！") ;
+		System.out.println("世界，你好！") ;
 	}
 }
