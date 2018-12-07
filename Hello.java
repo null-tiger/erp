ae6a867a6e3f1d4fb904aae251e00a6e4a753a40
mldn <mldnqa@163.com>
2018-12-07 23:53:24 +0800

    Last

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 		System.out.println("认真听课的孩子才是好孩子！") ;
 		System.out.println("不认真听课的孩子去后面站着！") ;
 		System.out.println("世界，你好！") ;
+		System.out.println(new.java.util.Date()) ;
 	}
 }
